618140e0600226198534eec992c0f32071c950bc
BenKreh <BenKrehbiel888@gmail.com>
2025-02-17 22:01:31 -0600

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,13 +1,53 @@
-# Program #2: Math Quiz
-# Write a program that gives simple math quizzes.  The program should display two random numbers to be added, such as
+#Ben Krehbiel
+#2/27/2025
+#did you do your math homework?
 
-#     247
 
-# + 129
 
-# ------
+import random as rn
+import operator
 
-# The program should allow the student to enter the answer.  
-# If the answer is correct, a message of congratulations should be displayed.  
-# If the answer is incorrect a message showing the correct answer should be displayed.  
-# The program must use a function that accomplishes part of the needed tasks.
+operator_functions = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
+}
+
+operators = ["+", "-", "*", "/"]
+correct = 0
+wrong = 0
+
+def get_question():
+    global correct
+    global wrong
+    for i in range(5):
+        rn_op = rn.choice(operators)
+        n1 = rn.randint(1, 1000)
+        n2 = rn.randint(1, 1000)
+
+        print(f'What is: {n1} {rn_op} {n2}?')
+
+        result = operator_functions[rn_op](n1, n2)
+        rounded_result = round(result, 2)
+        try:
+            user_input = float(input("Please enter your answer (round to 2 decimal places if needed): "))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+            continue
+
+        if user_input == rounded_result:
+            print("That is correct!")
+            correct += 1
+        else:
+            print(f"Incorrect. The correct answer is {rounded_result}.")
+            wrong += 1
+
+def yippie():
+    global correct
+    global wrong
+    print("You got ", correct, "right", "and", wrong, "wrong")
+
+
+get_question()
+yippie()
